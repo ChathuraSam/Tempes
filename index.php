fcec7b57b421f0da1a6ec3c481d77338fc13a79e
ChathuraSam <chathura.samarajeewa@gmail.com>
2019-04-22 22:06:11 +0530

    added index.php file content basics

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport"content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet"href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/
+	css/bootstrap.min.css">
+	<!-- Main CSS -->
+	<link rel="stylesheet"href="css/main.css">
+</head>
+<body>
+
+	<nav class="navbar navbar-toggleable-md bg-faded navbar-inverse bg-primary">
+		<button class="navbar-toggler navbar-toggler-right"type="button"data-toggle="collapse" 
+		data-target="#navbarNavDropdown"aria-controls="navbarNavDropdown"aria-expanded="false"
+		aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<a class="navbar-brand"href="#">Tempes</a>
+	<div class="collapse navbar-collapse"id="navbarNavDropdown">
+		<ul class="navbar-nav">
+			<li class="nav-item active">
+				<a class="nav-link"href="#">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link"href="#">Features</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link"href="#">Pricing</a>
+			</li>
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle"href="http://example.com"
+				id="navbarDropdownMenuLink"data-toggle="dropdown"aria-haspopup="true"ariaexpanded="false">
+				Dropdown link
+			</a>
+			<div class="dropdown-menu"aria-labelledby="navbarDropdownMenuLink">
+				<a class="dropdown-item"href="#">Action</a>
+				<a class="dropdown-item"href="#">Another action</a>
+				<a class="dropdown-item" href="#">Something else here</a>
+			</div>
+		</li>
+	</ul>
+</div>
+</nav>
+
+<div  class="container">
+	<div  class="row">
+		<div class="col-sm-6">Left Column
+			<form>
+				<div class="form-group">
+					<label for="exampleInputEmail1">Email address</label>
+					<input type="email"class="form-control"id="exampleInputEmail1"ariadescribedby="emailHelp"placeholder="Enter email">
+					<small id="emailHelp"class="form-text text-muted">We'll never share your email with 
+					anyone else.</small>
+				</div>
+				<div class="form-group">
+					<label for="exampleInputPassword1">Password</label>
+					<input type="password"class="form-control"id="exampleInputPassword1"
+					placeholder="Password">
+				</div>
+				<div class="checkbox">
+					<label>
+						<input type="checkbox"> Check me out
+					</label>
+				</div>
+				<button type="submit"class="btn btn-primary">Submit</button>
+			</form>
+		</div>
+		<div class="col-sm-6">Right Column</div>
+	</div>
+</div>
+<!-- jQuery first, then Tether, then Bootstrap JS. -->
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></
+script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.
+js"></script>
+<!-- Main JS -->
+<script  src="js/main.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tempes-Login</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+	<div class="background">
+		<div class="center">
+			<h1>Log in to see inside</h1>
+		</div>
+	</div>
+</body>
+</html>
