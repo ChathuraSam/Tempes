21b0a73eb2b0f5a128959ba27d596b14e6ca99b4
ChathuraSam <chathura.samarajeewa@gmail.com>
2019-04-30 16:06:24 +0530

    changed whole styles

--- index.php
+++ index.php
@@ -1,83 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<!-- Required meta tags -->
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<!-- <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> -->
+	<link rel="icon" href="assets/weather-icon.png" type="image/png" sizes="16x16">
+	<title>Tempes-Weather Search</title>
 	<meta charset="utf-8">
-	<meta name="viewport"content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet"href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/
-	css/bootstrap.min.css">
-	<!-- Main CSS -->
-	<link rel="stylesheet"href="css/main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
+
 <body>
-
-	<nav class="navbar navbar-toggleable-md bg-faded navbar-inverse bg-primary">
-		<button class="navbar-toggler navbar-toggler-right"type="button"data-toggle="collapse" 
-		data-target="#navbarNavDropdown"aria-controls="navbarNavDropdown"aria-expanded="false"
-		aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-	<a class="navbar-brand"href="#">Tempes</a>
-	<div class="collapse navbar-collapse"id="navbarNavDropdown">
-		<ul class="navbar-nav">
-			<li class="nav-item active">
-				<a class="nav-link"href="#">Home <span class="sr-only">(current)</span></a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link"href="#">Features</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link"href="#">Pricing</a>
-			</li>
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle"href="http://example.com"
-				id="navbarDropdownMenuLink"data-toggle="dropdown"aria-haspopup="true"ariaexpanded="false">
-				Dropdown link
-			</a>
-			<div class="dropdown-menu"aria-labelledby="navbarDropdownMenuLink">
-				<a class="dropdown-item"href="#">Action</a>
-				<a class="dropdown-item"href="#">Another action</a>
-				<a class="dropdown-item" href="#">Something else here</a>
-			</div>
-		</li>
-	</ul>
-</div>
-</nav>
-
-<div  class="container">
-	<div  class="row">
-		<div class="col-sm-6">Left Column
-			<form>
-				<div class="form-group">
-					<label for="exampleInputEmail1">Email address</label>
-					<input type="email"class="form-control"id="exampleInputEmail1"ariadescribedby="emailHelp"placeholder="Enter email">
-					<small id="emailHelp"class="form-text text-muted">We'll never share your email with 
-					anyone else.</small>
-				</div>
-				<div class="form-group">
-					<label for="exampleInputPassword1">Password</label>
-					<input type="password"class="form-control"id="exampleInputPassword1"
-					placeholder="Password">
-				</div>
-				<div class="checkbox">
-					<label>
-						<input type="checkbox"> Check me out
-					</label>
-				</div>
-				<button type="submit"class="btn btn-primary">Submit</button>
+	<div class="cont">
+		<div class="main">
+			<img src="assets/fogg-coffee-break.png" width="30%">
+			<h1>Weather Forecast</h1>
+			<form action="index.php" method="GET">
+				<input type="text" name="city" class="form-input" placeholder="City">
+				<input type="submit" class="btn" value="Find">
 			</form>
 		</div>
-		<div class="col-sm-6">Right Column</div>
 	</div>
-</div>
-<!-- jQuery first, then Tether, then Bootstrap JS. -->
-<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></
-script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.
-js"></script>
-<!-- Main JS -->
-<script  src="js/main.js"></script>
+	<div>Humidity: <?php echo $data->main->humidity; ?> %</div>
+	<div>Wind: <?php echo $data->wind->speed; ?> km/h</div>
+	<?php
+	if (isset($_GET['city'])) {
+		$city = $_GET['city'];
+		echo "***********";
+
+		$apiKey = "ff1ab9a051b88deba1cc6fa45581d638";
+		$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" . $city . "&lang=en&units=metric&APPID=" . $apiKey;
+
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		curl_setopt($ch, CURLOPT_VERBOSE, 0);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		$response = curl_exec($ch);
+
+		curl_close($ch);
+		$data = json_decode($response);
+		$currentTime = time();
+	} else {
+		echo "Nothin";
+	}
+	?>
 </body>
+
 </html>
